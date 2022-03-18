99976a0070731d0ad3f7659c145719ed9674b316
Florian Matter <florianmatter@gmail.com>
2022-03-18 15:43:47 +0100

    various package stuff

---
+++ README.md
@@ -0,0 +1,37 @@
+========
+pylacoan
+========
+
+
+.. image:: https://img.shields.io/pypi/v/pylacoan.svg
+        :target: https://pypi.python.org/pypi/pylacoan
+
+.. image:: https://img.shields.io/travis/fmatter/pylacoan.svg
+        :target: https://travis-ci.com/fmatter/pylacoan
+
+.. image:: https://readthedocs.org/projects/pylacoan/badge/?version=latest
+        :target: https://pylacoan.readthedocs.io/en/latest/?version=latest
+        :alt: Documentation Status
+
+
+
+
+Combine different corpus annotators
+
+
+* Free software: Apache Software License 2.0
+* Documentation: https://pylacoan.readthedocs.io.
+
+
+Features
+--------
+
+* TODO
+
+Credits
+-------
+
+This package was created with Cookiecutter_ and the `audreyr/cookiecutter-pypackage`_ project template.
+
+.. _Cookiecutter: https://github.com/audreyr/cookiecutter
+.. _`audreyr/cookiecutter-pypackage`: https://github.com/audreyr/cookiecutter-pypackage
---
+++ src/pylacoan/commands/new.py
@@ -0,0 +1,39 @@
+"""
+Initialize a new pylocan project.
+"""
+# credit to https://github.com/clld/clld/blob/05b9fb415fe52ec7573b537de41fd6711f75be97/src/clld/commands/create.py
+
+from cookiecutter.main import cookiecutter
+from cookiecutter.exceptions import OutputDirExistsException
+from clldutils.clilib import PathType
+import pathlib
+import pylacoan
+
+
+def register(parser):
+    parser.add_argument(
+        "-f",
+        "--force",
+        default=True,
+        help="Overwrite an existing project directory",
+        action="store_true",
+    )
+    parser.add_argument(
+        "--out",
+        help="Directory in which to create the skeleton",
+        type=pathlib.Path,
+        default=pathlib.Path("."),
+    )
+
+
+def run(args):
+    try:
+        cookiecutter(
+            str(pathlib.Path(pylacoan.__file__).parent / "project_template"),
+            overwrite_if_exists=args.force,
+            output_dir=str(args.out),
+        )
+    except OutputDirExistsException as e:
+        print(e)
+        print("Run with --force option to overwrite!")
+        raise ValueError()
